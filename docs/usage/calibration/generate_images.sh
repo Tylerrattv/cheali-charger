1c11438603307deeaeb115d4a4f64279065026f2
Paweł Stawicki <stawel+chealiCharger@gmail.com>
2016-07-30 17:59:12 +0200

    test

--- docs/usage/calibration/generate_images.sh
+++ docs/usage/calibration/generate_images.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 GEN=../../../utils/lcdScreenshotGenerator/generate.py
+GEN2=../../../utils/lcdScreenshotGenerator/generate2.py
 
 
 RESIZE=50%
 
-$GEN ">options "   "i_options.png"   $RESIZE
-$GEN ">calibrate " "i_calibrate.png" $RESIZE
-$GEN ">voltage "   "i_voltage.png"   $RESIZE
+$GEN ">options "        "i_options.png"   $RESIZE
+$GEN ">calibrate "      "i_calibrate.png" $RESIZE
+$GEN ">voltage "        "i_voltage.png"   $RESIZE
+$GEN ">Vin:    14.055V" "i_vin.png"       $RESIZE
+$GEN ">Vb1:     3.965V" "i_vb1.png"       $RESIZE
+$GEN ">Vb6:     3.970V" "i_vb6.png"       $RESIZE
+
+$GEN2 ">V1-6:   11.901V" " Vout:   11.901V"  "i_vb1-6_vout.png"    $RESIZE
+
